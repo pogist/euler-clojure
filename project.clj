d21c90b325a870852659c2d9e61aa2703be1925d
pogist <murilo.paixao.2@gmail.com>
2020-07-11 03:10:40 -0300

    chore(project.clj): fix metadata

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
-(defproject euler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject euler "0.0.1"
+  :url "https://github.com/pogist/projecteuler-clojure"
+  :description "My humble attempt to solve some euler's problems in clojure"
+  :license {:name "MIT" :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :main ^:skip-aot euler.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
